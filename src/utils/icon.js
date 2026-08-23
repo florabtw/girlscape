d90633a74ba7a7852d56e6c92cefda120cf80eff
Flora Moon <hello@flora.cool>
2026-08-23 16:43:37 -0700

    reverse clan icon rank order

--- src/utils/icon.js
+++ src/utils/icon.js
@@ -1,34 +1,34 @@
 const Ranks = [
-  "Burnt",
-  "Illusionist",
-  "Champion",
-  "Explorer",
-  "Specialist",
-  "Trickster",
-  "Zenyte",
-  "Pure",
-  "Adventurer",
-  "Artisan",
-  "Witch",
-  "Red_Topaz",
-  "Trialist",
-  "Completionist",
-  "Ignitor",
-  "Warlock",
-  "Dragonstone",
-  "Hero",
-  "Raider",
-  "Legacy",
-  "Seer",
-  "Onyx",
-  "Defiler",
   "Elite",
+  "Defiler",
+  "Onyx",
+  "Seer",
+  "Legacy",
+  "Raider",
+  "Hero",
+  "Dragonstone",
+  "Warlock",
+  "Ignitor",
+  "Completionist",
+  "Trialist",
+  "Red_Topaz",
+  "Witch",
+  "Artisan",
+  "Adventurer",
+  "Pure",
+  "Zenyte",
+  "Trickster",
+  "Specialist",
+  "Explorer",
+  "Champion",
+  "Illusionist",
+  "Burnt",
 ];
 
 export function getRankIcon({ summary }) {
   const rank = summary.rank.current;
   const rankName = Ranks[Math.max(rank - 1, 0)];
-  const fileName = `Rank_${rankName}.png`;
+  const fileName = `Clan_icon_${rankName}.png`;
   const path = `./src/images/ranks/${fileName}`;
   return { path, fileName };
 }
